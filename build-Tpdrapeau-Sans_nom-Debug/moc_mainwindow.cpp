57c2aae3801eea1202e5925a7ffe74f8b20bdd2f
Lyesnow <74401301+Lyesnow@users.noreply.github.com>
2021-02-12 10:09:08 +0100

    Add files via upload

---
+++ build-Tpdrapeau-Sans_nom-Debug/moc_mainwindow.cpp
@@ -0,0 +1,185 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'mainwindow.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.9.5)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../Tpdrapeau/mainwindow.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'mainwindow.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.9.5. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_MainWindow_t {
+    QByteArrayData data[19];
+    char stringdata0[274];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_MainWindow_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_MainWindow_t qt_meta_stringdata_MainWindow = {
+    {
+QT_MOC_LITERAL(0, 0, 10), // "MainWindow"
+QT_MOC_LITERAL(1, 11, 10), // "Drapeau_RT"
+QT_MOC_LITERAL(2, 22, 0), // ""
+QT_MOC_LITERAL(3, 23, 17), // "Drapeau_ALLEMAGNE"
+QT_MOC_LITERAL(4, 41, 16), // "Drapeau_AUTRICHE"
+QT_MOC_LITERAL(5, 58, 16), // "Drapeau_BELGIQUE"
+QT_MOC_LITERAL(6, 75, 14), // "Drapeau_FRANCE"
+QT_MOC_LITERAL(7, 90, 8), // "Hlayout1"
+QT_MOC_LITERAL(8, 99, 8), // "Hlayout2"
+QT_MOC_LITERAL(9, 108, 16), // "Drapeau_BULGARIE"
+QT_MOC_LITERAL(10, 125, 15), // "Drapeau_ESTONIE"
+QT_MOC_LITERAL(11, 141, 15), // "Drapeau_HONGRIE"
+QT_MOC_LITERAL(12, 157, 15), // "Drapeau_IRLANDE"
+QT_MOC_LITERAL(13, 173, 14), // "Drapeau_ITALIE"
+QT_MOC_LITERAL(14, 188, 16), // "Drapeau_LETTONIE"
+QT_MOC_LITERAL(15, 205, 16), // "Drapeau_LITUANIE"
+QT_MOC_LITERAL(16, 222, 18), // "Drapeau_LUXEMBOURG"
+QT_MOC_LITERAL(17, 241, 15), // "Drapeau_PAYSBAS"
+QT_MOC_LITERAL(18, 257, 16) // "Drapeau_ROUMANIE"
+
+    },
+    "MainWindow\0Drapeau_RT\0\0Drapeau_ALLEMAGNE\0"
+    "Drapeau_AUTRICHE\0Drapeau_BELGIQUE\0"
+    "Drapeau_FRANCE\0Hlayout1\0Hlayout2\0"
+    "Drapeau_BULGARIE\0Drapeau_ESTONIE\0"
+    "Drapeau_HONGRIE\0Drapeau_IRLANDE\0"
+    "Drapeau_ITALIE\0Drapeau_LETTONIE\0"
+    "Drapeau_LITUANIE\0Drapeau_LUXEMBOURG\0"
+    "Drapeau_PAYSBAS\0Drapeau_ROUMANIE"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_MainWindow[] = {
+
+ // content:
+       7,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      17,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    0,   99,    2, 0x0a /* Public */,
+       3,    0,  100,    2, 0x0a /* Public */,
+       4,    0,  101,    2, 0x0a /* Public */,
+       5,    0,  102,    2, 0x0a /* Public */,
+       6,    0,  103,    2, 0x0a /* Public */,
+       7,    0,  104,    2, 0x0a /* Public */,
+       8,    0,  105,    2, 0x0a /* Public */,
+       9,    0,  106,    2, 0x0a /* Public */,
+      10,    0,  107,    2, 0x0a /* Public */,
+      11,    0,  108,    2, 0x0a /* Public */,
+      12,    0,  109,    2, 0x0a /* Public */,
+      13,    0,  110,    2, 0x0a /* Public */,
+      14,    0,  111,    2, 0x0a /* Public */,
+      15,    0,  112,    2, 0x0a /* Public */,
+      16,    0,  113,    2, 0x0a /* Public */,
+      17,    0,  114,    2, 0x0a /* Public */,
+      18,    0,  115,    2, 0x0a /* Public */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        MainWindow *_t = static_cast<MainWindow *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->Drapeau_RT(); break;
+        case 1: _t->Drapeau_ALLEMAGNE(); break;
+        case 2: _t->Drapeau_AUTRICHE(); break;
+        case 3: _t->Drapeau_BELGIQUE(); break;
+        case 4: _t->Drapeau_FRANCE(); break;
+        case 5: _t->Hlayout1(); break;
+        case 6: _t->Hlayout2(); break;
+        case 7: _t->Drapeau_BULGARIE(); break;
+        case 8: _t->Drapeau_ESTONIE(); break;
+        case 9: _t->Drapeau_HONGRIE(); break;
+        case 10: _t->Drapeau_IRLANDE(); break;
+        case 11: _t->Drapeau_ITALIE(); break;
+        case 12: _t->Drapeau_LETTONIE(); break;
+        case 13: _t->Drapeau_LITUANIE(); break;
+        case 14: _t->Drapeau_LUXEMBOURG(); break;
+        case 15: _t->Drapeau_PAYSBAS(); break;
+        case 16: _t->Drapeau_ROUMANIE(); break;
+        default: ;
+        }
+    }
+    Q_UNUSED(_a);
+}
+
+const QMetaObject MainWindow::staticMetaObject = {
+    { &QMainWindow::staticMetaObject, qt_meta_stringdata_MainWindow.data,
+      qt_meta_data_MainWindow,  qt_static_metacall, nullptr, nullptr}
+};
+
+
+const QMetaObject *MainWindow::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *MainWindow::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_MainWindow.stringdata0))
+        return static_cast<void*>(this);
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 17)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 17;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 17)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 17;
+    }
+    return _id;
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
